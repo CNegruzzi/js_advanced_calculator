0dc38ba767cd96814096c843666077fc29770429
Cirillo Negruzzi <assus.12345nagi@gmail.com>
2024-11-14 11:37:55 +0100

    added division by 0

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -22,7 +22,13 @@ function makeCalculator() {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
     multiply: (a, b) => a * b,
-    divide: (a, b) => a / b,
+    divide: (a, b) => {
+      if (b !== 0) {
+        return a / b;
+      } else {
+        return a;
+      }
+    },
   };
 }
 
